d305b7083210deb4f5d42790f0f2e99fd072ab12
NyCodeGHG <nycode@protonmail.com>
2020-12-22 12:59:39 +0100

    Removed spigradle and added buildconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,27 @@
-import kr.entree.spigradle.kotlin.*
+import org.apache.commons.io.output.ByteArrayOutputStream
+import org.apache.tools.ant.filters.FixCrLfFilter
+import org.apache.tools.ant.filters.ReplaceTokens
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    id("kr.entree.spigradle")
-    id("com.github.johnrengelman.shadow")
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.gmazzo.buildconfig") version "2.0.2"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     jcenter()
-    // PaperMC Repository
-    papermc()
+    maven(url = "https://papermc.io/repo/repository/maven-public/")
 }
 
 val minecraft_version: String by project
 
 dependencies {
     // PaperMC Dependency
-    compileOnly(paper(minecraft_version))
+    compileOnly("com.destroystokyo.paper", "paper-api", "$minecraft_version-R0.1-SNAPSHOT")
 
     // Add your dependencies here
     // Examples
@@ -28,22 +29,39 @@ dependencies {
     // compileOnly("io.ktor", "ktor-client", "1.4.0") // Only used on compile time
 }
 
-// This is your plugin.yml
-spigot {
-    name = "ExamplePlugin"
-    authors = listOf("Me")
-    apiVersion = "1.16"
-    commands {
-        // Example command
-        create("example") {
-            description = "Example command"
-            permission = "example.permission"
-            aliases = emptyList()
-        }
+buildConfig {
+    className("BuildConfig")
+    packageName("$group.$name")
+    val commit = getGitHash()
+    val branch = getGitBranch()
+    buildConfigField("String", "GIT_COMMIT", "\"$commit\"")
+    buildConfigField("String", "GIT_BRANCH", "\"$branch\"")
+}
+
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine( "git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
     }
+    return stdout.toString("UTF-8").trim()
+}
+
+fun getGitBranch(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine( "git", "rev-parse", "--abbrev-ref", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString("UTF-8").trim()
 }
 
 tasks {
+    processResources {
+        filter(FixCrLfFilter::class)
+        filter(ReplaceTokens::class, "tokens" to mapOf("version" to project.version))
+        filteringCharset = "UTF-8"
+    }
     jar {
         // Disabled, because we use the shadowJar task for building our jar
         enabled = false
@@ -51,10 +69,9 @@ tasks {
     build {
         dependsOn(shadowJar)
     }
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
 }
--- src/main/kotlin/ExamplePlugin.kt
+++ src/main/kotlin/ExamplePlugin.kt
@@ -1,9 +1,7 @@
 package com.example.exampleplugin
 
-import kr.entree.spigradle.annotations.PluginMain
 import org.bukkit.plugin.java.JavaPlugin
 
-@PluginMain // important for plugin.yml generation
 class ExamplePlugin : JavaPlugin() {
 
     override fun onEnable() { }
